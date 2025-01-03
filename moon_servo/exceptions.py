263aac576093936901adbc1b78e46068074a0879
Deepak <deepakkumaryadav.ait@gmail.com>
2025-01-03 15:43:43 +0530

    Changes in package structure

--- moon_servo/exceptions.py
+++
@@ -1,7 +0,0 @@
-class MoonServoError(Exception):
-    """Base class for Moon Servo Motor exceptions."""
-    pass
-
-class ConnectionError(MoonServoError):
-    """Raised when Modbus connection fails."""
-    pass
