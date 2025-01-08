323f6b9962a8faf64218140d748fa038f6bc2948
SVRROBOTICS <swarag@svrrobotics.com>
2025-01-08 11:36:40 +0530

    commit

--- robomuse_motor_driver/src/motor_node.py
+++ robomuse_motor_driver/src/motor_node.py
@@ -37,14 +37,22 @@ class MotorControllerNode(Node):
             linear_velocity = msg.linear.x  # Forward/backward velocity
             angular_velocity = msg.angular.z  # Rotational velocity
 
+            self.get_logger().info(f"Received linear velocity: {linear_velocity}")
+            self.get_logger().info(f"Received angular velocity: {angular_velocity}")
+
             # Convert cmd_vel to motor commands
             left_motor_speed, right_motor_speed = self.calculate_motor_speeds(linear_velocity, angular_velocity)
 
             left_motor_speed = int(left_motor_speed)
             right_motor_speed = int(right_motor_speed)
             # Send commands to the motor driver
-            self.motor_driver.set_speed1(left_motor_speed, right_motor_speed)
-            self.motor_driver.set_speed2(left_motor_speed, right_motor_speed)
+            self.get_logger().info(f"Left motor speed: {left_motor_speed}")
+            self.get_logger().info(f"Right motor speed: {right_motor_speed}")
+
+            self.motor_driver.set_speed1(left_motor_speed)
+            self.motor_driver.set_speed2(right_motor_speed)
+
+
 
             self.get_logger().info(f"Set motor speeds: Left={left_motor_speed}, Right={right_motor_speed}")
         except Exception as e:
